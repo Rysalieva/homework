1c2a94eaf4501ca18f0e8d096242561ce1953782
Rysalieva <aizirekrysalieva05@gmail.com>
2024-03-23 00:15:33 +0600

    2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import About from "./Components/About/About";
+import Title from "./Components/Title/Title";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const titleData = {
+        title: "Заголовок",
+        subtitle: "Подзаголовок"
+    };
+
+    return (
+        <div>
+            <Title title={titleData.title} subtitle={titleData.subtitle} />
+            <About />
+        </div>
+    );
+};
 
 export default App;
---
+++ src/Components/Title/Title.js
@@ -0,0 +1,13 @@
+// Title.js
+import React from 'react';
+
+const Title = ({ title, subtitle }) => {
+    return (
+        <div>
+            <h1>{title}</h1>
+            <h3>{subtitle}</h3>
+        </div>
+    );
+};
+
+export default Title;
